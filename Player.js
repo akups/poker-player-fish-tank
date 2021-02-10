40d6c7d2cfc77dbfe72036d7bc9c29ac65f98332
Nadeem Jamali <nadeem@candis.io>
2021-02-10 14:09:56 +0100

    apply previous strategy

--- Player.js
+++ Player.js
@@ -1,3 +1,9 @@
+// const rules = [
+// {
+//   conditions: [],
+//   betValue: 0
+// }
+// ]
 function isFaceCard(card) {
   if (['a', 'k', 'q', 'j'].includes(card.rank.toLowerCase())) {
     return true;
@@ -20,23 +26,23 @@ class Player {
    * @param bet
    */
   static betRequest(gameState, bet) {
-    bet(1000)
-    return
     const players = gameState.players;
-    if (players) {
-      const ourTeam = players.find(function (p) { return p.name === 'Fish Tank' });
-      if (ourTeam && ourTeam.hole_cards.some(function (c) { return isFaceCard(c) })) {
-        const max = players.reduce(function (previous, player) {
-          return Math.max(previous, player.bet);
-        }, 0);
-        bet(max + 100);
-      } else {
-        bet(0)
-      }
-    } else {
+    if(!players){
       bet(300);
+      return 
+    }
+    
+    const ourTeam = players.find(function (p) { return p.name === 'Fish Tank' });
+    if (ourTeam && ourTeam.hole_cards.some(function (c) { return isFaceCard(c) })) {
+      const max = players.reduce(function (previous, player) {
+        return Math.max(previous, player.bet);
+      }, 0);
+      bet(max + 100);
+    } else {
+      bet(0);
     }
   }
+
   static showdown(gameState) {
   }
 }
