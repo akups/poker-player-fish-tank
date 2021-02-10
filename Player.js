c82383cc93d950316765c3a9ec60366fb61e5690
Akua <akua@candis.io>
2021-02-10 11:59:24 +0100

    strategy 2

--- Player.js
+++ Player.js
@@ -1,25 +1,36 @@
+const isFaceCard = (card) => {
+  if (['a', 'k', 'q', 'j'].includes(card.rank.toLower())) {
+    return true;
+  }
+  return false;
+}
 class Player {
-  static get VERSION() {
-    return '0.1';
-  }
-
-  static betRequest(gameState, bet) {
-    console.log('Hello')
-    const players = gameState.players;
-    if(players){
-      const max = players.reduce((previous, player
-        ) => {
-        return Math.max(previous, player.bet);
-      }, 0);
-      bet(max + 100);
-    }else{
-      bet(400);
+  static ofSameSuit(cardA, cardB) {
+    if (cardA.suit === cardB.suit) {
+      return true;
     }
-    
+    return;
   }
-
+  static get VERSION() {
+    return "0.1";
+  }
+  static betRequest(gameState, bet) {
+    const players = gameState.players;
+    if (players) {
+      const ourTeam = players.find(p => p.name === "Fish Tank");
+      if (ourTeam && ourTeam.length && ourTeam.hole_cards.some(c => isFaceCard(c))) {
+        const max = players.reduce((previous, player) => {
+          return Math.max(previous, player.bet);
+        }, 0);
+        bet(max + 100);
+      } else {
+        bet(0)
+      }
+    } else {
+      bet(300);
+    }
+  }
   static showdown(gameState) {
   }
 }
-
 module.exports = Player;
