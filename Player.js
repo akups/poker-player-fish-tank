16c248671d037bc27a7e1663d14364475764bdb4
Akua <akua@candis.io>
2021-02-10 12:45:31 +0100

    startegy 2 enhanced

--- Player.js
+++ Player.js
@@ -1,25 +1,36 @@
+const isFaceCard = (card) => {
+  if(['a', 'k', 'q', 'j'].includes(card.rank.toLowerCase())) {
+   return true;
+}
+  return false;
+}
 class Player {
+  static ofSameSuit(cardA, cardB) {
+    if(cardA.suit === cardB.suit){
+      return true;
+}
+    return;
+}
   static get VERSION() {
     return '0.1';
-  }
-
-  static betRequest(gameState, bet) {
-    console.log('Hello')
-    const players = gameState.players;
-    if (players) {
-      const max = players.reduce((previous, player
-      ) => {
-        return Math.max(previous, player.bet);
-      }, 0);
-      bet(max + 100);
-    } else {
-      bet(400);
-    }
-
-  }
-
-  static showdown(gameState) {
-  }
 }
-
+  static betRequest(gameState, bet) {
+    const players = gameState.players;
+    if(players){
+      const ourTeam = players.find(p => p.name === 'Fish Tank');
+      if(ourTeam && ourTeam.hole_cards.some(c => isFaceCard(c))){
+        const max = players.reduce((previous, player) => {
+          return Math.max(previous, player.bet);
+}, 0);
+        bet(max + 100);
+}else{
+        bet(0)
+}
+}else{
+      bet(300);
+}
+}
+  static showdown(gameState) {
+}
+}
 module.exports = Player;
---
+++ Player.test.js
@@ -0,0 +1,62 @@
+const Player = require('./Player');
+
+describe('Player', () => {
+  describe('betRequest', () => {
+    it('should bet when a face card exist', () => {
+      const gameState = {
+        players: [
+          {
+            'name': 'Fish Tank',
+            hole_cards: [
+              {rank: 'A', suit: 'spades'}
+            ],
+            bet: 0
+          },
+          {
+            'name': 'Team A',
+            hole_cards: [
+              {rank: 'A', suit: 'spades'}
+            ],
+            bet: 5
+          }
+        ]
+      }
+
+      const bet = jest.fn();
+
+      Player.betRequest(gameState, bet);
+
+      expect(bet).toHaveBeenCalledWith(105);
+
+    });
+
+    it('should not bet when no face cards exist', () => {
+      const gameState = {
+        players: [
+          {
+            'name': 'Fish Tank',
+            hole_cards: [
+              {rank: '1', suit: 'spades'}
+            ],
+            bet: 0
+          },
+          {
+            'name': 'Team A',
+            hole_cards: [
+              {rank: 'A', suit: 'spades'}
+            ],
+            bet: 5
+          }
+        ]
+      }
+
+      const bet = jest.fn();
+
+      Player.betRequest(gameState, bet);
+
+      expect(bet).toHaveBeenCalledWith(0);
+
+    });
+  });
+
+});
