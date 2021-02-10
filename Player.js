68f3707f0130ab8f77d6c92f1ff4c5aed946bf61
Akua <akua@candis.io>
2021-02-10 13:38:32 +0100

    new startegy

--- Player.js
+++ Player.js
@@ -1,36 +1,36 @@
-const isFaceCard = (card) => {
-  if(['a', 'k', 'q', 'j'].includes(card.rank.toLowerCase())) {
-   return true;
-}
+function isFaceCard(card) {
+  if (['a', 'k', 'q', 'j'].includes(card.rank.toLowerCase())) {
+    return true;
+  }
   return false;
 }
 class Player {
   static ofSameSuit(cardA, cardB) {
-    if(cardA.suit === cardB.suit){
+    if (cardA.suit === cardB.suit) {
       return true;
-}
+    }
     return;
-}
+  }
   static get VERSION() {
     return '0.1';
-}
+  }
   static betRequest(gameState, bet) {
     const players = gameState.players;
-    if(players){
-      const ourTeam = players.find(p => p.name === 'Fish Tank');
-      if(ourTeam && ourTeam.hole_cards.some(c => isFaceCard(c))){
-        const max = players.reduce((previous, player) => {
+    if (players) {
+      const ourTeam = players.find(function (p) { return p.name === 'Fish Tank' });
+      if (ourTeam && ourTeam.hole_cards.some(function (c) { return isFaceCard(c) })) {
+        const max = players.reduce(function (previous, player) {
           return Math.max(previous, player.bet);
-}, 0);
+        }, 0);
         bet(max + 100);
-}else{
+      } else {
         bet(0)
-}
-}else{
+      }
+    } else {
       bet(300);
-}
-}
+    }
+  }
   static showdown(gameState) {
-}
+  }
 }
 module.exports = Player;
